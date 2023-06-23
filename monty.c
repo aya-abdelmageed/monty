3dc9c16dcf8e059f3efaa6067f22cea7d9ceb341
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-06-23 09:13:37 -0700

    add task

---
+++ monty.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+global_var var;
+
+/**
+ * main - function for monty prog
+ * @c: the numbers of arguments
+ * @v: opcode file (arguments)
+ * Return: 0
+ */
+
+int main(int c, char **v)
+{
+	stack_t *st;
+
+	st = NULL;
+
+	if (c != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	read_file(v[1], &st);
+
+	_free(st);
+	return (0);
+}
